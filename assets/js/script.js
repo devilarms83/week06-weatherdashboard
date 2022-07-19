d4848c0367a6a559cd3005764b5a8bd30b7f50b0
Alpha Bravo <bautista.albert@gmail.com>
2022-07-18 23:44:18 -0500

    Reverting to validation commit 8f62e2b

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,14 +15,18 @@ $(document).ready(function () {
             $('#validation').text("")
         } else {
             $('#mainDisplay').removeAttr("hidden")
+            searchVal.toUpperCase
+            searchHist.push(searchVal)
+            searchHist.sort()
+            localStorage.setItem('prevSearch', JSON.stringify(searchHist))
             $('#cityInput').val("")
             $('#validation').text("")
             $('#prevCities').empty()
             for (var x = 0; x < searchHist.length; x++){
                 addCity(searchHist[x])
             }
+
             currentConditions(searchVal)
-            storeLocal(searchVal)
         }
     })
 
@@ -41,29 +45,27 @@ $(document).ready(function () {
                 $('#cityInput').val("")
             } else {
                 $('#mainDisplay').removeAttr("hidden")
+                searchHist.push(sVal)
+                searchHist.sort()
+                localStorage.setItem('prevSearch', JSON.stringify(searchHist))
                 $('#cityInput').val("")
                 $('#prevCities').empty()
                 for (var x = 0; x < searchHist.length; x++){
                     addCity(searchHist[x])
                 }
-                currentConditions(sVal)
-                storeLocal(sVal)
-            }
+            currentConditions(sVal)
+        }
         }
       })
 
     $('#clearBtn').on("click", function () {
         clearCurrent()
-        localStorage.removeItem('prevSearch')
-        clearMaindiv()
-    })
-
-    function clearMaindiv() {
         $('#cityDiv').attr("hidden",true)
         $('#forecastDisplay').attr("hidden",true)
+        localStorage.removeItem('prevSearch')
         $('#prevCities').empty()
         $('#validation').text("")
-    }
+    })
 
     var searchHist = JSON.parse(localStorage.getItem("prevSearch"))|| []
     if (searchHist.length > 0) {
@@ -93,13 +95,6 @@ $(document).ready(function () {
         $('#uvStr').text("")
         $('#forecastDays').empty()
     }
-
-    function storeLocal(cityTxt){
-        cityTxt.toUpperCase
-        searchHist.push(cityTxt)
-        searchHist.sort()
-        localStorage.setItem('prevSearch', JSON.stringify(searchHist))
-    }
     
     function currentConditions(searchText) {
         clearCurrent()
@@ -108,37 +103,33 @@ $(document).ready(function () {
         $('#cityName').text(searchText)
 
         $.ajax({
-            url: "https://api.openweathermap.org/geo/1.0/direct?q=" + searchText + "&appid=c7936d34a1de114ab154db84bfde1ac8",
+            url: "http://api.openweathermap.org/geo/1.0/direct?q=" + searchText + "&appid=c7936d34a1de114ab154db84bfde1ac8",
             method: "GET",
         }).then(function (currentData) {
-            // console.log(currentData)
-            // console.log(currentData.length)
+            console.log(currentData.status)
+            
+            var lat = currentData[0].lat
+            var lon = currentData[0].lon
 
-            if (currentData.length == 0) {
-                clearCurrent()
-                clearMaindiv()
+            if (currentData.status === 'undefined' || currentData.status === null) {
                 $('#validation').text("Search empty or city not found!")
                 $('#validation').css({ 'color': 'red','font-style': 'italic'})
-                return
+                alert("Stop!")
             } else {
-                
-                var lat = currentData[0].lat
-                var lon = currentData[0].lon
-
                 $.ajax({
                     url: "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&appid=c7936d34a1de114ab154db84bfde1ac8&units=imperial&exclude=minutely,hourly,alerts",
                     method: "GET",
                 }).then(function(conditionData){
                     console.log(conditionData)
-                    $('#cityName').append(" (" + moment().format("M/DD/YYYY") + ") " + "<img src='https://openweathermap.org/img/wn/" + conditionData.current.weather[0].icon + ".png'/>")
-
+                    $('#cityName').append(" (" + moment().format("M/DD/YYYY") + ") " + "<img src='http://openweathermap.org/img/wn/" + conditionData.current.weather[0].icon + ".png'/>")
+    
                     $('#tempText').append("Temp: " + conditionData.current.temp + "\u00B0 F")
                     $('#windText').append("Wind: " + conditionData.current.wind_speed + " MPH")
                     $('#humText').append("Humidity: " + conditionData.current.humidity + " %")
                     $('#uvStr').append("UV Index: <span class='uvColor'>" + conditionData.current.uvi + "</span>")
                     
                     var uvIndex = conditionData.current.uvi
-
+    
                     if (uvIndex <= 2) {
                         $('.uvColor').css({"background-color": "green", "color": "white","padding": "0px 10px"})
                     } else if (uvIndex >= 3 && uvIndex <= 7) {
@@ -146,12 +137,13 @@ $(document).ready(function () {
                     } else {
                         $('.uvColor').css({"background-color": "red", "color": "white","padding": "0px 10px"})
                     }
-
+    
                     for (var y = 0; y < 5; y++){
-                        $("#forecastDays").append("<li class='px-4 py-2 bg-dark text-light text-left'><h4>" + moment().add(y+1, 'd').format("M/DD/YYYY") + "</h4><p><img src='https://openweathermap.org/img/wn/" + conditionData.daily[y].weather[0].icon + ".png'/></p><p>Temp: " + conditionData.daily[y].temp.day + "\u00B0 F</p><p>Wind: " + conditionData.daily[y].wind_speed + " MPH</p><p>Humidity: " + conditionData.daily[y].humidity + " %</p></li>")
+                        $("#forecastDays").append("<li class='px-4 py-2 bg-dark text-light text-left'><h4>" + moment().add(y+1, 'd').format("M/DD/YYYY") + "</h4><p><img src='http://openweathermap.org/img/wn/" + conditionData.daily[y].weather[0].icon + ".png'/></p><p>Temp: " + conditionData.daily[y].temp.day + "\u00B0 F</p><p>Wind: " + conditionData.daily[y].wind_speed + " MPH</p><p>Humidity: " + conditionData.daily[y].humidity + " %</p></li>")
                     }
                 })
-            }         
+            }
+
         })
     }
 
